75b6623fc95a389a1a88e99fb6f380085dbdbcce
Serhii Kazban <kazbanserhii@gmail.com>
2026-05-15 17:20:18 +0300

    add weather controller

---
+++ Backend/WeatherAPI/Controllers/WeatherController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Weather.Models;
+using Weather.Services;
+
+namespace WeatherAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+
+        private readonly IWeatherService _weatherService;
+
+        public WeatherController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet("{city}")]
+        public async Task<ActionResult<WeatherResponse>> GetWeather(string city)
+        {
+            var weather = await _weatherService.GetWeather(city);
+            return Ok(weather);
+        }
+    }
+}

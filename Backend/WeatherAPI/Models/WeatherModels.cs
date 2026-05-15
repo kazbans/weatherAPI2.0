280c3c01a44fe56aa75458d605e47b182b30a194
Serhii Kazban <kazbanserhii@gmail.com>
2026-05-15 16:49:08 +0300

    add  weather models

---
+++ Backend/WeatherAPI/Models/WeatherModels.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Weather.Models;
+
+public class WeatherResponse
+{
+    public string City { get; set; }
+    public double Temperature { get; set; }
+    public string Message { get; set; }
+}
+
+public class OpenWeatherResponse
+{
+    [JsonPropertyName("main")]
+    public MainInfo Main { get; set; }
+}
+
+public class MainInfo
+{
+    [JsonPropertyName("temp")]
+    public double Temp { get; set; }
+}

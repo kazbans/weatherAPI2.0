d2d0014129d7c288316a9a7769830a2c4d884e8f
Serhii Kazban <kazbanserhii@gmail.com>
2026-05-17 18:20:49 +0300

    add routing

--- Frontend/WeatherUI/src/app/app.routes.ts
+++ Frontend/WeatherUI/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'weather/lviv', pathMatch: 'full' },
+    { path: 'weather/:city', component: WeatherComponent }
+];
--- Frontend/WeatherUI/src/app/weather/weather.component.ts
+++ Frontend/WeatherUI/src/app/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
 import { WeatherResponse } from '../interfaces/weather.interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-weather',
@@ -10,12 +11,13 @@ import { WeatherResponse } from '../interfaces/weather.interface';
 })
 export class WeatherComponent implements OnInit {
   private weatherService = inject(WeatherService);
+  private route = inject(ActivatedRoute);
 
   weatherData: WeatherResponse | null = null;
 
   ngOnInit() {
-    
-    const city = 'Lviv';
+
+    const city = this.route.snapshot.paramMap.get('city') || 'lviv';
 
     this.weatherService.getWeather(city).subscribe((response) => {
       this.weatherData = response;

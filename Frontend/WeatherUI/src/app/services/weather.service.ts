3fe0a3986d06863684ef22ac8637196ad3fad195
Serhii Kazban <kazbanserhii@gmail.com>
2026-05-17 13:45:14 +0300

    add weather service for api calls

---
+++ Frontend/WeatherUI/src/app/services/weather.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { WeatherResponse } from '../interfaces/weather.interface';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string){
+    return this.http.get<WeatherResponse>(`https://localhost:7127/api/Weather/${city}`);
+  }
+}
